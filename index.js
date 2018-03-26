ef12ee24ac16e535502777eb9f12fc96d8c189e5
ben awad <benawad97@gmail.com>
2018-03-26 12:35:50 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const { GraphQLServer } = require("graphql-yoga");
+const fetch = require("node-fetch");
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+    getPerson(id: Int!): Person
+  }
+
+  type Planet {
+    name: String
+    rotation_period: String
+    orbital_period: String
+    films: [Film]
+  }
+
+  type Film {
+    title: String
+    episode_id: Int
+    opening_crawl: String
+    director: String
+    producer: String
+    release_date: String
+  }
+
+  type Person {
+    name: String
+    height: String
+    mass: String
+    hair_color: String
+    skin_color: String
+    eye_color: String
+    birth_year: String
+    gender: String
+    films: [Film]
+    homeworld: Planet
+  }
+`;
+
+const resolveFilms = parent => {
+  const promises = parent.films.map(async url => {
+    const response = await fetch(url);
+    return response.json();
+  });
+
+  return Promise.all(promises);
+};
+
+const resolvers = {
+  Planet: {
+    films: resolveFilms
+  },
+  Person: {
+    homeworld: async parent => {
+      const response = await fetch(parent.homeworld);
+      return response.json();
+    },
+    films: resolveFilms
+  },
+  Query: {
+    hello: (_, { name }) => `Hello ${name || "World"}`,
+    getPerson: async (_, { id }) => {
+      const response = await fetch(`https://swapi.co/api/people/${id}/`);
+      return response.json();
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
